bd87e2f2d0b4901ea3f2b3972c803e1b88a676f0
wuyouliaoxi <105232189+wuyouliaoxi@users.noreply.github.com>
2022-12-03 18:51:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup
 
+setup(
+   name='snowflake',
+   version='1.0',
+   description='A useful module',
+   author='Siyuan Mei',
+   author_email='siyuan.mei@fau.de',
+   packages=['snowflake'],  #same as name
+   install_requires=['numpy', 'turtles'], #external packages as dependencies
+)
