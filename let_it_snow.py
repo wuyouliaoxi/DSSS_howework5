e1ef2a2bfc0adf23197aa22fa8691f9824b38509
Siyuan <623990701@qq.com>
2022-12-03 18:15:48 +0100

    commit to branch

--- let_it_snow.py
+++ let_it_snow.py
@@ -1,20 +1,21 @@
-import turtle
+from turtle import *
 import numpy as np
+import turtle
+from random import randint
 
 
 def main(speed=0, bg_color="grey"):
     # create Turtle object
     turtle_screen = turtle.Screen()
     myTurtle = turtle.Turtle()
-    
+
     # set speed to 'fastest = 0'
     myTurtle.speed(speed)
     # change background color
     turtle_screen.bgcolor(bg_color)
-  
- 
-    """TODO: define different colors here"""
 
+    """TODO: define different colors here"""
+    colormode(255)
 
     for _ in range(10):
         # define some params
@@ -22,7 +23,7 @@ def main(speed=0, bg_color="grey"):
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
-
+        myTurtle.color(randint(0, 255), randint(0, 255), randint(0, 255))
         # Go to the start position of the snowflake
         myTurtle.penup()
         myTurtle.goto(pos[0], pos[1])
@@ -33,6 +34,7 @@ def main(speed=0, bg_color="grey"):
             snowflake_branch(size, myTurtle)
             myTurtle.left(45)
 
+    turtle.done()
 
 def snowflake_branch(size, myTurtle):
     # This function draws one branch of the snowflake.
